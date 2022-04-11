1b7453e0ae487b3a256db6b17f0870fb31d8ecaa
Misael-CG <50430378+Misael-CG@users.noreply.github.com>
2022-04-11 18:17:43 -0500

    firstComit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Headers from "./componets/Headers";
+import Formulario from "./componets/Formulario";
+import ListadoPacientes from "./componets/ListadoPacientes";
+
+function App() {
+  return (
+    <>
+      <Headers />
+      <Formulario />
+      <ListadoPacientes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/componets/Headers.jsx
@@ -0,0 +1,5 @@
+function Headers() {
+  return <h1>Desde Header</h1>;
+}
+
+export default Headers;
